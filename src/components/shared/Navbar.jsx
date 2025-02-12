9edbdb3a541405d55019ea3c57d97d12a46bddda
Jaman Shah <jamanshah5400@gmail.com>
2025-02-13 00:36:21 +0600

    active link learning finished

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,15 +1,37 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const activeLink = (path) => {
+    return path === pathname
+      ? "bg-green-500 text-white px-4 py-2 rounded"
+      : "px-4 py-2";
+  };
+
+  const links = [
+    { name: "Home", route: "/" },
+    { name: "Counter", route: "/counter" },
+    { name: "Products", route: "/products" },
+    { name: "Dashboard", route: "/dashboard" },
+    { name: "Gallery", route: "/gallery" },
+  ];
+
   return (
-    <div className="flex justify-center gap-8 border p-2 bg-blue-500">
-      <Link href="/">Home</Link>
-      <Link href="/counter">Counter</Link>
-      <Link href="/products">Products</Link>
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/gallery">Gallery</Link>
-    </div>
+    <nav className="flex justify-center gap-4 border p-3 bg-blue-500 text-white shadow-md">
+      {links.map((link) => (
+        <Link
+          key={link.route}
+          href={link.route}
+          className={activeLink(link.route)}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
   );
 };
 
